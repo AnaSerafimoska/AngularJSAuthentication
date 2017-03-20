bb28723d2eb042b9a759ff6fc95c0ad5e169a877
AnaSerafimoska <serafimoska.ana@gmail.com>
2017-03-20 23:45:09 +0100

    AngularJSBackend
    AngularJSAuthentication Backend App

---
+++ AngularJSAuthentication/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularJSAuthentication.Controllers
+{
+    [RoutePrefix("api/DepartmentController")]
+    public class DepartmentController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            CompanyDBEntities cb = new CompanyDBEntities();
+            return Ok(cb.FetchAllDepartments());
+        }
+    }
+}
